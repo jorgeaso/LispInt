fe2cfc784ef31a3bf150a842cb1aeaafa86aa772
jorgejaso <jorgejaso@172.30.38.252>
2013-07-01 13:15:42 +0100

    Trying to implement arithmetic expressions. There's an error when running the program. (still pending to solve) Third fix to the GUI with all panels finally showing.

--- src/LispIntGUI.java
+++ src/LispIntGUI.java
@@ -10,17 +10,16 @@ import java.io.*;
  */
 
 public class LispIntGUI extends JFrame implements ActionListener {
-    /** GUI Buttons */
+    /** GUI Buttons **/
     private JButton selectButton, executeButton, exitButton;
     /** GUI JTextFields */
     private JTextField sourceInbox;
-    /** Display of lisp code and output */
-    private JTextArea displayFile, displayOutput;
+    /** GUI TextArea to display source lisp code and output */
+    private JTextArea displayCode, displayOutput;
+    /** GUI Label output */
+    private JLabel OutputLabel;
     /** Name of input file */
     private final String SrcInFile = "/Users/jorgejaso/NetBeansProjects/LispInt/src/arith.lisp";    
-    String top="";
-    String middle="";
-    String bottom="";
     String lispFile;
 	
     public LispIntGUI() {
@@ -42,11 +41,7 @@ public class LispIntGUI extends JFrame implements ActionListener {
 					String line = in.nextLine();
 					System.out.println(line);
 				}
-				System.out.println(top); // test
-				System.out.println(middle); // test
-				System.out.println(bottom); // test
 				reader.close();
-				//System.out.print(top+"\n"+middle+"\n"+bottom);
 			}
 			catch (IOException e) {
 				System.out.println("File not found");
@@ -55,61 +50,62 @@ public class LispIntGUI extends JFrame implements ActionListener {
 		
 	}
 
-	/**
-	 * JPanel containing labels and text field to specify source path and Select Button
-	 */
-	public void layoutTop() {
-		// instantiate panel for bottom of display
-		JPanel topJPanel = new JPanel(new GridLayout(1, 3));
-                topJPanel.setBackground(Color.yellow);
-		// label to specify source path
-		JLabel srcLabel = new JLabel("Enter Source File Path:");
-		topJPanel.add(srcLabel);
-		// textfield to enter source path
-                sourceInbox = new JTextField();
-		topJPanel.add(sourceInbox);
-		//JPanel panel1 = new JPanel();
-		// button to select file
-                selectButton = new JButton("Select");
-		selectButton.addActionListener(this);
-		//panel1.add(selectButton);
-		topJPanel.add(selectButton);
-                //topJPanel.add(panel1);
-                add(topJPanel, BorderLayout.NORTH);
-	}
+    /**
+    * JPanel containing labels and text field to specify source path and Select Button
+    **/
+    public void layoutTop() {
+            // instantiate panel for bottom of display
+            JPanel topJPanel = new JPanel(new GridLayout(1, 3));
+            topJPanel.setBackground(Color.yellow);
+            // label to specify source path
+            JLabel srcLabel = new JLabel("Enter Source File Path:");
+            topJPanel.add(srcLabel);
+            // textfield to enter source path
+            sourceInbox = new JTextField();
+            topJPanel.add(sourceInbox);
+            //JPanel panel1 = new JPanel();
+            // button to select file
+            selectButton = new JButton("Select");
+            selectButton.addActionListener(this);
+            //panel1.add(selectButton);
+            topJPanel.add(selectButton);
+            //topJPanel.add(panel1);
+            add(topJPanel, BorderLayout.NORTH);
+    }
         
-        // JPanel containing JTextArea and Execute Button
-        public void layoutMiddle() {
-		JPanel middleJPanel = new JPanel();
-                middleJPanel.setBackground(Color.red);
-                displayFile = new JTextArea();
-                displayFile.setFont(new Font("Courier", Font.PLAIN, 14));
-                middleJPanel.add(displayFile);
-                add(displayFile, BorderLayout.NORTH);
-                
-		executeButton = new JButton("Execute");
-		executeButton.addActionListener(this);
-		middleJPanel.add(executeButton);
-                add(middleJPanel, BorderLayout.CENTER);
-	}
+    // JPanel containing JTextArea and Execute Button
+    public void layoutMiddle() {
+            JPanel middleJPanel = new JPanel(new GridLayout(2, 1));
+            middleJPanel.setBackground(Color.red);
+            displayCode = new JTextArea();
+            displayCode.setFont(new Font("Courier", Font.PLAIN, 14));
+            middleJPanel.add(displayCode);
+
+            executeButton = new JButton("Execute");
+            executeButton.setEnabled(false);
+            executeButton.addActionListener(this);
+            middleJPanel.add(executeButton);
+            
+            add(middleJPanel, BorderLayout.CENTER);
+    }
         
 
-        /**
-	 * JPanel containing Label, JTextArea and Exit Button
-	 */
-	public void layoutBottom() {
-		JPanel bottomJPanel = new JPanel();
-                bottomJPanel.setBackground(Color.blue);
-                displayOutput = new JTextArea();
-                displayOutput.setFont(new Font("Courier", Font.PLAIN, 14));
-                bottomJPanel.add(displayOutput);
-		//add(displayOutput, BorderLayout.NORTH);
-		exitButton = new JButton("Exit");
-		exitButton.addActionListener(this);
-		bottomJPanel.add(exitButton);
-                
-                add(bottomJPanel, BorderLayout.SOUTH);
-	}
+    /**
+     * JPanel containing Label, JTextArea and Exit Button
+     */
+    public void layoutBottom() {
+            JPanel bottomJPanel = new JPanel(new GridLayout(3, 1));
+            bottomJPanel.setBackground(Color.blue);
+            displayOutput = new JTextArea();
+            displayOutput.setFont(new Font("Courier", Font.PLAIN, 14));
+            bottomJPanel.add(displayOutput);
+            //add(displayOutput, BorderLayout.NORTH);
+            exitButton = new JButton("Exit");
+            exitButton.addActionListener(this);
+            bottomJPanel.add(exitButton);
+
+            add(bottomJPanel, BorderLayout.SOUTH);
+    }
         
     	/**
 	 * Process button clicks.
