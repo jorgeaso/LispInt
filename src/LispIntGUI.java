239d6fefc4a1bf607785f1d458a9d07d49242e1f
jorgejaso <jorgejaso@172.30.38.252>
2013-07-01 11:41:14 +0100

    Trying to implement arithmetic expressions. There's an error when running the program. (still pending to solve) Second version of the GUI.

--- src/LispIntGUI.java
+++ src/LispIntGUI.java
@@ -10,31 +10,30 @@ import java.io.*;
  */
 
 public class LispIntGUI extends JFrame implements ActionListener {
-        /** GUI Buttons */
-        private JButton selectButton, executeButton, exitButton;
-    	/** GUI JTextFields */
-	private JTextField pathIn;
-
-	/** Display of lisp code and output */
-	private JTextArea displayFile, displayOutput;
-        
-        /** Name of input file */
-	private final String SrcInFile = "/Users/jorgejaso/NetBeansProjects/LispInt/src/arith.lisp";    
-	String top="";
-	String middle="";
-	String bottom="";
-	String lispFile;
+    /** GUI Buttons */
+    private JButton selectButton, executeButton, exitButton;
+    /** GUI JTextFields */
+    private JTextField sourceInbox;
+    /** Display of lisp code and output */
+    private JTextArea displayFile, displayOutput;
+    /** Name of input file */
+    private final String SrcInFile = "/Users/jorgejaso/NetBeansProjects/LispInt/src/arith.lisp";    
+    String top="";
+    String middle="";
+    String bottom="";
+    String lispFile;
 	
-   public LispIntGUI() {
+    public LispIntGUI() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Lisp Interpreter");
 		setSize(700, 300);
                 layoutTop();
+                layoutMiddle();
                 layoutBottom();	
     }
         
-        // Read Source File and Display
-        public void DisplaySource() {
+    // Read Source File and Display
+    public void DisplaySource() {
 		{
 			try {
 				FileReader reader = new FileReader("/Users/jorgejaso/NetBeansProjects/LispInt/src/arith.lisp");
@@ -43,7 +42,6 @@ public class LispIntGUI extends JFrame implements ActionListener {
 					String line = in.nextLine();
 					System.out.println(line);
 				}
-				//System.out.println(classReport); // test
 				System.out.println(top); // test
 				System.out.println(middle); // test
 				System.out.println(bottom); // test
@@ -58,39 +56,58 @@ public class LispIntGUI extends JFrame implements ActionListener {
 	}
 
 	/**
-	 * adds labels, text fields and buttons to bottom of GUI
+	 * JPanel containing labels and text field to specify source path and Select Button
 	 */
 	public void layoutTop() {
 		// instantiate panel for bottom of display
 		JPanel topJPanel = new JPanel(new GridLayout(1, 3));
-		// label, text field and button
+                topJPanel.setBackground(Color.yellow);
+		// label to specify source path
 		JLabel srcLabel = new JLabel("Enter Source File Path:");
 		topJPanel.add(srcLabel);
-		pathIn = new JTextField();
-		topJPanel.add(pathIn);
-		JPanel panel1 = new JPanel();
-		selectButton = new JButton("Select");
+		// textfield to enter source path
+                sourceInbox = new JTextField();
+		topJPanel.add(sourceInbox);
+		//JPanel panel1 = new JPanel();
+		// button to select file
+                selectButton = new JButton("Select");
 		selectButton.addActionListener(this);
-		panel1.add(selectButton);
-		topJPanel.add(panel1);
+		//panel1.add(selectButton);
+		topJPanel.add(selectButton);
+                //topJPanel.add(panel1);
                 add(topJPanel, BorderLayout.NORTH);
 	}
+        
+        // JPanel containing JTextArea and Execute Button
+        public void layoutMiddle() {
+		JPanel middleJPanel = new JPanel();
+                middleJPanel.setBackground(Color.red);
+                displayFile = new JTextArea();
+                displayFile.setFont(new Font("Courier", Font.PLAIN, 14));
+                middleJPanel.add(displayFile);
+                add(displayFile, BorderLayout.NORTH);
+                
+		executeButton = new JButton("Execute");
+		executeButton.addActionListener(this);
+		middleJPanel.add(executeButton);
+                add(middleJPanel, BorderLayout.CENTER);
+	}
+        
 
         /**
-	 * adds button to bottom of GUI
+	 * JPanel containing Label, JTextArea and Exit Button
 	 */
 	public void layoutBottom() {
 		JPanel bottomJPanel = new JPanel();
-                displayFile = new JTextArea();
-                displayFile.setFont(new Font("Courier", Font.PLAIN, 14));
-                add(displayFile, BorderLayout.NORTH);
-                
+                bottomJPanel.setBackground(Color.blue);
                 displayOutput = new JTextArea();
                 displayOutput.setFont(new Font("Courier", Font.PLAIN, 14));
-		add(displayOutput, BorderLayout.CENTER);
+                bottomJPanel.add(displayOutput);
+		//add(displayOutput, BorderLayout.NORTH);
 		exitButton = new JButton("Exit");
 		exitButton.addActionListener(this);
 		bottomJPanel.add(exitButton);
+                
                 add(bottomJPanel, BorderLayout.SOUTH);
 	}
         
